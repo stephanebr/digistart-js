09c3998a59486a38bb6c51a6cb9a4fcb6a98b24a
Stéphane Brulé <brule.stephane@yahoo.fr>
2021-10-13 12:04:41 +0200

    Test

---
+++ session4/2-objets/main.js
@@ -0,0 +1,50 @@
+/* LES OBJETS */
+console.log('Bienvenue dans la console javascript');
+console.log('Vous êtes dans les exercices sur les objets (objects)');
+
+// ------ TODO 1 ------
+
+// 1. Déclarer une variable student qui contient un objet avec les propriétés et valeurs suivantes :
+// - name : Votre nom
+// - age : Votre age
+// - city: Votre ville
+
+
+
+// 2. Afficher dans la console l'objet student.
+
+
+
+
+// ------ TODO 2 ------
+var user = {
+  name: 'John',
+  age: 30
+};
+
+// 1. Ajouter une propriété (clé) "city" de valeur "Paris" à l'objet user.
+
+
+
+// 2. Afficher dans la console l'âge de John.
+
+
+
+// 3. Remplacer la valeur de "city" avec "Nice".
+
+
+
+
+// ------ TODO 3 ------
+var users = [user, { name: 'Emma', age: 32, city: 'London' }];
+
+// 1. Afficher dans la console l'objet dont le name est "Emma".
+
+
+
+// 2. Afficher dans la console "John", en accédant aux élements du tableau
+// puis à la propriété de l'objet.
+
+
+
+
