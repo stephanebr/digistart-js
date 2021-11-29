0be46eff6e2131d24e0f098c3f599bcb5ffb332f
Stéphane Brulé <brule.stephane@yahoo.fr>
2021-11-29 18:35:36 +0100

    Création des dossiers

---
+++ Tenzin-Jampel-PHUNTSANG/session1/1-variables/main.js
@@ -0,0 +1,12 @@
+console.log('Bienvenue dans votre premier exercice sur JavaScript !');
+
+// TODO: ÉTAPE 1
+// 1. Afficher dans la console le mois actuel en assignant la bonne valeur à la variable `month` ci-dessous.
+// Libre à vous de choisir le format : soit le numéro, soit le nom du mois.
+
+var month;
+
+// 2. Afficher dans la console le mois prochain
+// en modifiant la variable `month` ci-dessus,
+// donc sans supprimer le code de l'étape 1 !
+
---
+++ Tenzin-Jampel-PHUNTSANG/session2/1-methodes/main.js
@@ -0,0 +1,40 @@
+/* LES MÉTHODES */
+console.log("Bienvenue dans la console javascript");
+console.log("Vous êtes dans les exercices sur les méthodes");
+
+// ------ TODO 1 - Les Strings------ 
+// TODO: ÉTAPE 1
+// Écrire une variable du nom de votre choix, et de valeur de type String.
+
+
+
+
+// TODO: ÉTAPE 2
+// Afficher sa longueur dans la console grâce à la propriété "length".
+
+
+
+
+// ------ TODO 2 ------ 
+// TODO: ÉTAPE 1
+// Ajouter un espace au début ou à la fin de votre string.
+
+
+
+
+
+// TODO: ÉTAPE 2
+// Afficher sa longueur dans la console, et observez la différence.
+
+
+
+
+// ------ TODO 3 ------ 
+// TODO: ÉTAPE 1
+// Écrire une nouvelle variable du nom de votre choix, et de valeur de type String.
+
+
+
+
+// TODO: ÉTAPE 2
+// Afficher dans la console sa valeur en MAJUSCULES grâce à la méthode "toUpperCase".
